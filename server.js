c3976cea8280016b9caf01c32c84c1bd9b731430
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2024-04-21 11:31:42 +0200

    CHANGES, comment cron task

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const swing_routes = require('./routes/swingRoute.js')
 const scalp_routes = require('./routes/scalpingRoute.js')
-const cron = require('node-cron')
-const cronTVA = require('./cron/TradingviewAlertsDate.js')
+// const cron = require('node-cron')
+// const cronTVA = require('./cron/TradingviewAlertsDate.js')
 
 app.listen(3000, () => {
     console.log('server is listening on port 3000')
